ac45362cb48b8f5f7ebb2bc0c911b608fa4db6ef
alopez022 <108113287+alopez022@users.noreply.github.com>
2023-05-19 09:22:34 +0000

    Domain name first attempt

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".mx"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extensions[l]);
+          document.getElementById("domainNamesList").innerHTML +=
+            "<p>" + pronoun[i] + adj[j] + noun[k] + extensions[l] + "</p>";
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
